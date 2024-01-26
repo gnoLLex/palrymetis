fabde70704a0bc8034e370b596e6bb59b18b4f4d
Lucas Engelmann <gnollex@protonmail.com>
2024-01-26 16:49:09 +0100

    subscriptable recording

--- palrymetis/recorder.py
+++ palrymetis/recorder.py
@@ -1,37 +1,98 @@
 import threading
 import signal
 import spdlog
+import time
 
 from tqdm import tqdm
 from queue import Queue
+from typing import Optional, List
 
+from polymetis_pb2 import RobotState
 from palrymetis.panda import Panda
 
+ROBOT_STATE_MEMBERS = [
+        'joint_positions',
+        'joint_torques_computed',
+        'joint_velocities',
+        'motor_torques_desired',
+        'motor_torques_external',
+        'motor_torques_measured',
+        'prev_command_successful',
+        'prev_controller_latency_ms',
+        'prev_joint_torques_computed',
+        'prev_joint_torques_computed_safened',
+        'timestamp'
+]
+
+GRIPPER_STATE_MEMBERS = [
+        'error_code',
+        'is_grasped',
+        'is_moving',
+        'prev_command_successful',
+        'timestamp',
+        'width'
+]
+
+# TODO: gripper state subscriptions
 class Recorder:
-    def __init__(self, robot: Panda, running):
+    def __init__(
+        self,
+        robot: Panda,
+        running: callable,
+        subscriptions: Optional[List[str]] = None,
+        hz: float = 10.0
+    ):
+        """
+        """
         self.logger = spdlog.ConsoleLogger("recorder")
         self.robot = robot
-        self.recordings = []
+        self.recordings = {}
         self.thread = None
         self.running = running
+        self.hz = hz
+        
+        subscriptions = subscriptions if subscriptions else ROBOT_STATE_MEMBERS
+
+        for sub in subscriptions:
+            if not sub in ROBOT_STATE_MEMBERS: # and not sub in GRIPPER_STATE_MEMBERS 
+                self.logger.error(f"No subscriptable member called {sub} found.\nTry {ROBOT_STATE_MEMBERS}") # or {GRIPPER_STATE_MEMBERS}")
+
+        self.subscriptions = subscriptions
+
+        for sub in self.subscriptions:
+            self.recordings[sub] = []
 
     def start(self):
+        """
+        """
         self.thread = threading.Thread(target=self._record)
         self.thread.start()
 
     def _record(self):
+        last_timestamp = 0
         while self.running():
-            self.recordings.append(self.robot.robot.get_robot_state())
+            state = self.robot.robot.get_robot_state()
+
+            # skip if we receive the same state
+            if not last_timestamp == state.timestamp:
+                for sub in self.subscriptions:
+                    self.recordings[sub].append(getattr(state, sub))
+                time.sleep(1 / self.hz)
+
+            last_timestamp = state.timestamp
 
     def stop(self):
+        """
+        """
         if self.thread:
             self.thread.join()
             self.thread = None
 
-    # TODO: proper fileformat and subscribtable properties
     def save(self):
+        """
+        """
         import os
-        import json
+        import pandas
         from datetime import datetime
 
         save_dir = "outputs/recordings/"
@@ -40,10 +101,11 @@ class Recorder:
             os.makedirs(save_dir)
     
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        filename = f"{timestamp}.txt"
+        filename = f"{timestamp}.csv"
         full_path = os.path.join(save_dir, filename)
 
         self.logger.info(f"Saving to {full_path}")
-        with open(full_path, 'w') as f:
-            for recording in tqdm(self.recordings):
-                f.write(f"{recording.timestamp}_{recording.joint_positions}\n")
+        if 'timestamp' in self.subscriptions:
+            self.recordings['timestamp'] = [ts.seconds * 1e9 + ts.nanos for ts in self.recordings['timestamp']] 
+        pandas.DataFrame(self.recordings).to_csv(full_path, float_format='%32.f')
+
--- palrymetis/teleoperation.py
+++ palrymetis/teleoperation.py
@@ -59,8 +59,8 @@ GRIPPER_LOWER_BOUND = 0.01
 GRIPPER_UPPER_BOUND = 0.075
 GRIPPER_SPEED = 1.0
 GRIPPER_FORCE = 20.0
-GRIPPER_GRASP_THRESHOLD = 0.05
-GRIPPER_OPEN_THRESHOLD = 0.06
+GRIPPER_GRASP_THRESHOLD = 0.04
+GRIPPER_OPEN_THRESHOLD = 0.05
 
 class Teleoperation:
     def __init__(self, operated_panda: Panda, imitator_panda: Panda):
@@ -75,7 +75,6 @@ class Teleoperation:
         # Reset imitator to operated robots current positions
         self.logger.info("Setting up imitator")
         self.imitator_panda.robot.move_to_joint_positions(self.operated_panda.robot.get_joint_positions())
-        self.imitator_panda.gripper.goto(self.operated_panda.gripper.get_state().width, GRIPPER_SPEED, 0.1)
         self.logger.info("Finished setting up imitator")
 
         self.logger.info("Sending torch policies")
@@ -91,7 +90,7 @@ class Teleoperation:
 
     def run(self):
         self.running = True
-        last_command = "none"
+        last_command = "move"
         last_issued = -1
         while self.running:
             if not self.operated_panda.robot.is_running_policy():
@@ -107,37 +106,27 @@ class Teleoperation:
                 self.logger.warn(f"Trying to resend {self.imitator_panda.name}'s policy")
                 self.imitator_panda.robot.send_torch_policy(self.imitation_controller, blocking=False)
 
-            # mirror gripper state
-            operated_gripper_state = self.operated_panda.gripper.get_state()
-            desired_gripper_width = operated_gripper_state.width
+            # mirror gripper state for now only binary (grasping fully or moving to fully open)
+            desired_gripper_width = self.operated_panda.gripper.get_state().width
 
-            imitator_gripper_state = self.imitator_panda.gripper.get_state()
-            imitator_gripper_width = imitator_gripper_state .width
-
-            if imitator_gripper_state.is_moving:
-                continue
-
-
-            if desired_gripper_width < GRIPPER_GRASP_THRESHOLD and not last_command == "grasp":
+            if desired_gripper_width < GRIPPER_GRASP_THRESHOLD and last_command == "move":
                 self.logger.debug("Grasping")
                 self.imitator_panda.gripper.grasp(
+                    grasp_width=GRIPPER_LOWER_BOUND,
                     speed=GRIPPER_SPEED,
                     force=GRIPPER_FORCE,
-                    grasp_width=GRIPPER_LOWER_BOUND,
                     epsilon_inner=0.01,
                     epsilon_outer=0.2,
-                    blocking=False,
                 )
 
                 last_command = "grasp"
 
-            elif desired_gripper_width > GRIPPER_OPEN_THRESHOLD:
+            elif desired_gripper_width > GRIPPER_OPEN_THRESHOLD and last_command == "grasp":
                 self.logger.debug("Moving")
                 self.imitator_panda.gripper.goto(
                         width=GRIPPER_UPPER_BOUND,
                         speed=GRIPPER_SPEED,
                         force=GRIPPER_FORCE,
-                        blocking=False,
                 )
 
                 last_command = "move"
